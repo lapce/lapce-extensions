88a77b7b5da10ee331d636bb1937bc2bb67a53a9
CoffeeMovedCoder <tiagodinis33@gmail.com>
2022-09-15 17:38:51 +0000

    style: made navbar always colorful

--- marketplace/src/components/Sticky.svelte
+++ marketplace/src/components/Sticky.svelte
@@ -7,7 +7,6 @@
 		let lastScrollY = 0;
 		window.onscroll = () => {
 			show = (window.scrollY > lastScrollY);
-			top = (window.scrollY < 30);
 			lastScrollY = window.scrollY;
 		}
 	})
@@ -22,21 +21,15 @@
 	.scrolled {
 		transform: translate(0,calc(-100% - 1rem))
 	}
-	.top {
-		background: white;
-	}
 	.sticky {
 		width: 100%;
 		position: fixed;
 		padding: 10px;
-		transition: transform 0.5s ease;
         background: linear-gradient(to right top, #6ca0e0, #63a9e6, #5ab2eb, #2eb9e7, #00bfdd, #00c3cd, #10c6ba);
+		transition: 1s ease;
 	}
-    :global(body){
-        height: 200vh;
-    }
 </style>
 
-<div class="sticky" bind:this={element} class:top={top} class:scrolled={show}>
+<div class="sticky" bind:this={element} class:scrolled={show}>
 	<slot/>
 </div>
