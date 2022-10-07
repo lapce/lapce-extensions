8f4b025e6f0dd13f4baf1b7053d9ab17a33e04b0
Tiago Dinis <tiagodinis33@gmail.com>
2022-10-07 18:19:59 +0100

    fix: remove owner and repo fields from plugins table

--- migrations/2022-09-15-173933_plugins-table/up.sql
+++ migrations/2022-09-15-173933_plugins-table/up.sql
@@ -2,8 +2,5 @@ create table plugins (
     id serial not null primary key,
     -- ID of the user that published this plugin
     user_id bigint not null,
-    owner varchar(600) not null,
-    -- Repository name
-    repo varchar(600) not null,
     CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users (id)
 )
